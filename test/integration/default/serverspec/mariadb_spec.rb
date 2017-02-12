a2195aec21acda9a5487c4bd068bdd24650a6e67
akinobu-tani <akinobu.x.tani@gmail.com>
2017-02-12 12:31:40 +0900

    add spec

--- test/integration/default/serverspec/mariadb_spec.rb
+++ test/integration/default/serverspec/mariadb_spec.rb
@@ -20,3 +20,21 @@ log_files.each do |file|
     it { should exist }
   end
 end
+
+describe command('yum install -y logrotate') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('logrotate -fv /etc/logrotate.d/mysql') do
+  its(:exit_status) { should eq 0 }
+end
+
+log_files.each do |file|
+  describe file(file) do
+    it { should be_mode 644 }
+  end
+
+  describe file("#{file}-#{Time.now.strftime('%Y%m%d')}") do
+    it { should exist }
+  end
+end

789f6ef4b7c2da14bf2e0945aa04a1f8367a8dc6
akinobu-tani <akinobu.x.tani@gmail.com>
2017-02-12 11:38:27 +0900

    add mariadb configure

--- test/integration/default/serverspec/mariadb_spec.rb
+++ test/integration/default/serverspec/mariadb_spec.rb
@@ -2,6 +2,10 @@ require 'serverspec'
 
 set :backend, :exec
 
+def log_files
+  %w(/var/log/mysql/error.log /var/log/mysql/slow.log)
+end
+
 describe command('mysql --version') do
   its(:exit_status) { should eq 0 }
 end
@@ -9,4 +13,10 @@ end
 describe service('mariadb') do
   it { should be_running }
   it { should be_enabled }
+end
+
+log_files.each do |file|
+  describe file(file) do
+    it { should exist }
+  end
 end

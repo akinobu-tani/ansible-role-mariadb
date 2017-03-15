3fec6de149f796f72d1b9277e4c2fdd8601f41d5
akinobu-tani <akinobu.x.tani@gmail.com>
2017-03-15 11:15:22 +0900

    amazon linux でも動くように修正

--- test/integration/default/serverspec/mariadb_spec.rb
+++ test/integration/default/serverspec/mariadb_spec.rb
@@ -10,7 +10,7 @@ describe command('mysql --version') do
   its(:exit_status) { should eq 0 }
 end
 
-describe service('mariadb') do
+describe service('mysql') do
   it { should be_running }
   it { should be_enabled }
 end
